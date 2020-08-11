eb10b75a070fb2333a376a6c55a411972a52dc66
Greg Lloyd <gergdll@gmail.com>
2020-08-11 16:32:29 -0700

    big fix

--- harmony_frontend/src/config.js
+++ harmony_frontend/src/config.js
@@ -1,11 +1,11 @@
 export const baseUrl =
                 process.env.REACT_APP_BASEURL ||
                 process.env.NODE_ENV === "production"
-                    ? 'https://harmony-fullstack.herokuapp.com/'
+                    ? 'https://harmony-fullstack.herokuapp.com'
                     : 'http://localhost:8081'
 
 export const wsUrl =
                 process.env.REACT_APP_WS_URL ||
                 process.env.NODE_ENV === "production"
-                    ? 'https://harmony-fullstack.herokuapp.com/'
+                    ? 'https://harmony-fullstack.herokuapp.com'
                     : 'ws://localhost:8081'
