cd73949c8f2ca18c9ec07176e7ac148e43bd69ca
Greg Lloyd <gergdll@gmail.com>
2020-10-21 15:56:52 -0700

    start refactor

---
+++ harmony_frontend/src/components/cs2.js
@@ -0,0 +1,58 @@
+import React, {useState} from "react";
+import {Button, Modal, Form} from "react-bootstrap"
+import { createServer } from "../actions/ServerActions";
+import { useDispatch, useSelector } from "react-redux";
+
+
+const CreateServer = (props) => {
+  const [show, setShow] = useState(false);
+  const [serverName, setServerName] = useState("")
+  const userName = useSelector((state) => state.authentication.user.userName);
+  const userId = useSelector((state) => state.authentication.user.id);
+  const dispatch= useDispatch()
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const server = await dispatch(createServer(serverName, userId))
+    await console.log(props.servers.server)
+    await console.log(server)
+    await props.setServers([...props.servers.server, server])
+    await console.log(props.servers)
+    await handleClose()
+  };
+
+  const updateServerName= (e) => setServerName(e.target.value)
+
+  return (
+    <>
+      <Button variant="secondary" onClick={handleShow}>
+        Create Server
+      </Button>
+
+      <Modal show={show} onHide={handleClose} animation={false}>
+        <Modal.Header closeButton>
+          <Modal.Title>Create A Server!</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+            <Form onSubmit={handleSubmit}>
+                <Form.Label>Enter Server Name</Form.Label>
+                <Form.Control type="text" placeholder="Enter a name for your server" value = {serverName} onChange={updateServerName} />
+            </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" type="submit" onClick={handleSubmit}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+};
+
+export default CreateServer;
